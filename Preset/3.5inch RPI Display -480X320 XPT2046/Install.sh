79b4e8d6faf7507e19de10952052b91525a715e2
TPM-28 <felicien.latour@hotmail.com>
2023-05-01 19:07:33 +0200

    xorg

---
+++ Preset/3.5inch RPI Display -480X320 XPT2046/Install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+echo 
+echo -e "\033[31m == Confirmation... == \033[0m"
+confirm()
+{
+    read -r -p "${1} [Y/n] " answer
+    case "$answer" in
+        [yY][eE][sS]|[yY]) 
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+if confirm "Are you sure to install the program ?"; then
+    sudo apt-get update & sudo apt-get full-upgrade -y
+    echo "The software will install..."
+    sudo apt-get install xserver-xorg-input-evdev xserver-xorg xinput-calibrator xorg unclutter chromium-browser git -y
+    sudo cp -rf /usr/share/X11/xorg.conf.d/10-evdev.conf /usr/share/X11/xorg.conf.d/45-evdev.conf
+    sudo rm /home/pi/.profile
+    sudo rm /home/pi/.xinitrc
+    cd /home/pi/
+    sudo wget https://raw.githubusercontent.com/felix068/Working_Raspi_Kiosk/main/.profile
+    cd /
+    sudo wget https://raw.githubusercontent.com/felix068/Working_Raspi_Kiosk/main/st.sh
+    echo -e "\033[31m The program setting your config file \033[0m"
+    sudo rm /boot/config.txt
+    cd /boot/
+    sudo wget "https://raw.githubusercontent.com/felix068/WP_Kiosk_Raspi/main/Preset/3.5inch%20RPI%20Display%20-480X320%20XPT2046/config.txt"    
+    echo -e "\033[31m Setting Xorg (X11) \033[0m"
+    sleep 3
+    sudo nano /etc/X11/xorg.conf.d/98-dietpi-disable_dpms.conf
+    echo -e "\033[31m Setting your screen resolution and chromium argument \033[0m"
+    cd /home/pi/
+    sudo rm .xinitrc
+    sudo wget "https://raw.githubusercontent.com/felix068/WP_Kiosk_Raspi/main/Preset/3.5inch%20RPI%20Display%20-480X320%20XPT2046/.xinitrc"
+    echo -e "\033[31m The operation was done ! \033[0m"
+    cd /home/pi/
+    wget "https://raw.githubusercontent.com/felix068/WP_Kiosk_Raspi/main/Preset/3.5inch%20RPI%20Display%20-480X320%20XPT2046/Gitsc-install.sh"
+    bash Gitsc-install.sh
+else
+    echo "The operation was canceled by the user."
+fi
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# Raspi_RDP_Client
+# WP_Kiosk_Raspi
+
+Wp kiosk allows to show webpage on your lcd display for raspberry pi , or your hdmi display
+with touch compatibility (Xorg)
+
+## Install Manual Setting :
+
+```
+bash <(wget -qO- https://raw.githubusercontent.com/felix068/Work_Raspi_Kiosk/main/Install.sh)
+```
+
+## Install 3.5inch RPI Display -480X320 XPT2046 :
+
+```
+bash <(wget -qO- https://tinyurl.com/4h5cr4zm)
+```
+After the execution of this command, your RasPi restart automatically and after reboot execute the following command :
+
+(Warning: This command restarts your RasPi)
+```
+bash <(wget -qO- https://tinyurl.com/yfdfjxf4)
+```
+
+
+## Install 5inch HDMI LCD V2 -800X480 XPT2046 :
+
+```
+bash <(wget -qO- https://tinyurl.com/4nehdfhv)
+```
+
+## Configure Touch
+
+```
+sudo nano /etc/X11/xorg.conf.d/98-dietpi-disable_dpms.conf
+```
